9e03e5b53bc72be0537d2d00bacce71420af1ec1
wueliton <paulo.wueliton@hotmail.com>
2022-04-06 09:16:25 -0300

    Definido user agent

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -4,4 +4,8 @@ export const apiService = axios.create({
   responseType: "arraybuffer",
   responseEncoding: "binary",
   validateStatus: (status) => true,
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3694.0 Safari/537.36 Chrome-Lighthouse",
+  },
 });
